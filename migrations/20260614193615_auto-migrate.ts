28a0cc30a6170da11afe640e449e76bfbb109f1a
Beeno Tung <aabbcc1241@yahoo.com.hk>
2026-06-15 03:37:41 +0800

    init: setup db files using quick-erd

---
+++ migrations/20260614193615_auto-migrate.ts
@@ -0,0 +1,105 @@
+import { Knex } from 'knex'
+
+// prettier-ignore
+export async function up(knex: Knex): Promise<void> {
+  if (!(await knex.schema.hasTable('user'))) {
+    await knex.schema.createTable('user', table => {
+      table.increments('id')
+      table.text('server').notNullable()
+      table.text('user').notNullable()
+      table.timestamps(false, true)
+    })
+  }
+
+  if (!(await knex.schema.hasTable('chat'))) {
+    await knex.schema.createTable('chat', table => {
+      table.increments('id')
+      table.integer('user_id').unsigned().notNullable().references('user.id')
+      table.text('name').notNullable()
+      table.boolean('is_group').notNullable()
+      table.boolean('is_read_only').notNullable()
+      table.integer('unread_count').notNullable()
+      table.integer('timestamp').notNullable()
+      table.boolean('archived').notNullable()
+      table.boolean('pinned').notNullable()
+      table.boolean('is_muted').notNullable()
+      table.integer('mute_expiration').notNullable()
+      table.integer('last_message_id').nullable()
+    })
+  }
+
+  if (!(await knex.schema.hasTable('message'))) {
+    await knex.schema.createTable('message', table => {
+      table.increments('id')
+      table.integer('chat_id').unsigned().notNullable().references('chat.id')
+      table.text('api_id').notNullable()
+      table.integer('ack').notNullable()
+      table.boolean('has_media').notNullable()
+      table.text('body').notNullable()
+      table.text('type').notNullable()
+      table.integer('timestamp').notNullable()
+      table.integer('from_user_id').unsigned().notNullable().references('user.id')
+      table.integer('to_user_id').unsigned().notNullable().references('user.id')
+      table.text('device_type').notNullable()
+      table.boolean('is_forwarded').notNullable()
+      table.integer('forwarding_score').notNullable()
+      table.boolean('is_status').notNullable()
+      table.boolean('is_starred').notNullable()
+      table.boolean('from_me').notNullable()
+      table.boolean('has_quoted_message').notNullable()
+      table.boolean('has_reaction').notNullable()
+      table.json('vcards').nullable()
+      table.json('mentioned_ids').nullable()
+      table.json('group_mentions').nullable()
+      table.boolean('is_gif').notNullable()
+      table.json('links').nullable()
+      table.json('poll_options').nullable()
+      table.json('poll_votes').nullable()
+    })
+  }
+
+  if (!(await knex.schema.hasTable('group'))) {
+    await knex.schema.createTable('group', table => {
+      table.increments('id')
+      table.integer('group_user_id').unsigned().notNullable().references('user.id')
+      table.integer('creation_time').notNullable()
+      table.integer('owner_user_id').unsigned().notNullable().references('user.id')
+      table.text('subject').notNullable()
+      table.integer('subject_time').notNullable()
+      table.text('desc').notNullable()
+      table.text('desc_id').notNullable()
+      table.integer('desc_time').notNullable()
+      table.integer('desc_owner_user_id').unsigned().notNullable().references('user.id')
+      table.boolean('membership_approval_mode').notNullable()
+      table.text('member_add_mode').notNullable()
+      table.boolean('suspended').notNullable()
+      table.boolean('terminated').notNullable()
+      table.boolean('is_parent_group').notNullable()
+      table.boolean('is_parent_group_closed').notNullable()
+      table.integer('parent_group_id').unsigned().notNullable().references('group.id')
+      table.json('pending_participants').nullable()
+      table.json('past_participants').nullable()
+      table.timestamps(false, true)
+    })
+  }
+
+  if (!(await knex.schema.hasTable('group_participants'))) {
+    await knex.schema.createTable('group_participants', table => {
+      table.increments('id')
+      table.integer('group_id').unsigned().notNullable().references('group.id')
+      table.integer('user_id').unsigned().notNullable().references('user.id')
+      table.boolean('is_admin').notNullable()
+      table.boolean('is_super_admin').notNullable()
+      table.timestamps(false, true)
+    })
+  }
+}
+
+// prettier-ignore
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.dropTableIfExists('group_participants')
+  await knex.schema.dropTableIfExists('group')
+  await knex.schema.dropTableIfExists('message')
+  await knex.schema.dropTableIfExists('chat')
+  await knex.schema.dropTableIfExists('user')
+}
---
+++ src/proxy.ts
@@ -0,0 +1,134 @@
+/**
+ * This file is auto generated, do not edit it manually.
+ *
+ * update command: npm run db:update
+ */
+
+import { proxySchema } from 'better-sqlite3-proxy'
+import { db } from './db'
+
+export type User = {
+  id?: null | number
+  server: string
+  user: string
+}
+
+export type Chat = {
+  id?: null | number
+  user_id: number
+  user?: User
+  name: string
+  is_group: boolean
+  is_read_only: boolean
+  unread_count: number
+  timestamp: number
+  archived: boolean
+  pinned: boolean
+  is_muted: boolean
+  mute_expiration: number
+  last_message_id: null | number
+}
+
+export type Message = {
+  id?: null | number
+  chat_id: number
+  chat?: Chat
+  api_id: string
+  ack: number
+  has_media: boolean
+  body: string
+  type: string
+  timestamp: number
+  from_user_id: number
+  from_user?: User
+  to_user_id: number
+  to_user?: User
+  device_type: string
+  is_forwarded: boolean
+  forwarding_score: number
+  is_status: boolean
+  is_starred: boolean
+  from_me: boolean
+  has_quoted_message: boolean
+  has_reaction: boolean
+  vcards: null | string // json
+  mentioned_ids: null | string // json
+  group_mentions: null | string // json
+  is_gif: boolean
+  links: null | string // json
+  poll_options: null | string // json
+  poll_votes: null | string // json
+}
+
+export type Group = {
+  id?: null | number
+  group_user_id: number
+  group_user?: User
+  creation_time: number
+  owner_user_id: number
+  owner_user?: User
+  subject: string
+  subject_time: number
+  desc: string
+  desc_id: string
+  desc_time: number
+  desc_owner_user_id: number
+  desc_owner_user?: User
+  membership_approval_mode: boolean
+  member_add_mode: string
+  suspended: boolean
+  terminated: boolean
+  is_parent_group: boolean
+  is_parent_group_closed: boolean
+  parent_group_id: number
+  parent_group?: Group
+  pending_participants: null | string // json
+  past_participants: null | string // json
+}
+
+export type GroupParticipants = {
+  id?: null | number
+  group_id: number
+  group?: Group
+  user_id: number
+  user?: User
+  is_admin: boolean
+  is_super_admin: boolean
+}
+
+export type DBProxy = {
+  user: User[]
+  chat: Chat[]
+  message: Message[]
+  group: Group[]
+  group_participants: GroupParticipants[]
+}
+
+export let proxy = proxySchema<DBProxy>({
+  db,
+  tableFields: {
+    user: [],
+    chat: [
+      /* foreign references */
+      ['user', { field: 'user_id', table: 'user' }],
+    ],
+    message: [
+      /* foreign references */
+      ['chat', { field: 'chat_id', table: 'chat' }],
+      ['from_user', { field: 'from_user_id', table: 'user' }],
+      ['to_user', { field: 'to_user_id', table: 'user' }],
+    ],
+    group: [
+      /* foreign references */
+      ['group_user', { field: 'group_user_id', table: 'user' }],
+      ['owner_user', { field: 'owner_user_id', table: 'user' }],
+      ['desc_owner_user', { field: 'desc_owner_user_id', table: 'user' }],
+      ['parent_group', { field: 'parent_group_id', table: 'group' }],
+    ],
+    group_participants: [
+      /* foreign references */
+      ['group', { field: 'group_id', table: 'group' }],
+      ['user', { field: 'user_id', table: 'user' }],
+    ],
+  },
+})
